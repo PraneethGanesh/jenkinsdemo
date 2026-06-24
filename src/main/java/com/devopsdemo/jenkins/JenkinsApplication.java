ee7980362685b3b03c98670adab8c9dbc6a50c5a
Praneeth Ganesh <gpraneeth20@gmail.com>
2026-06-24 11:41:32 +0530

    created a spring project

---
+++ src/main/java/com/devopsdemo/jenkins/JenkinsApplication.java
@@ -0,0 +1,21 @@
+package com.devopsdemo.jenkins;
+
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsApplication {
+    public static Logger logger= LoggerFactory.getLogger(JenkinsApplication.class);
+    @PostConstruct
+    void init(){
+        logger.info("JenkinsApplication init started");
+    }
+    public static void main(String[] args) {
+        logger.info("JenkinsApplication execute started");
+        SpringApplication.run(JenkinsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devopsdemo/jenkins/JenkinsApplicationTests.java
@@ -0,0 +1,19 @@
+package com.devopsdemo.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class JenkinsApplicationTests {
+
+    Logger logger= LoggerFactory.getLogger(JenkinsApplicationTests.class);
+    @Test
+    void contextLoads() {
+        logger.info("test is executing");
+        assertEquals(true,true);
+    }
+
+}
